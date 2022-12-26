01bc311bec4b15c5be484911b41b1062a4027b43
Kevyn Gorito <kevy4job@gmail.com>
2022-12-26 04:44:33 -0300

    botão doubleWeight inserido e falta o método do mesmo

--- youtube-crud/src/app/views/views.component.ts
+++ youtube-crud/src/app/views/views.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTable } from '@angular/material/table';
+import { elementAt } from 'rxjs';
 import { ElementDialogComponent } from '../shared/element-dialog/element-dialog.component';
 
 export interface PeriodicElement {
@@ -8,19 +9,20 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  doubleWeight: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', doubleWeight: 0},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', doubleWeight:  0},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', doubleWeight: 0},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', doubleWeight: 0},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', doubleWeight: 0},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', doubleWeight: 0},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', doubleWeight: 0},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', doubleWeight: 0},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', doubleWeight: 0},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', doubleWeight: 0},
 ];
 @Component({
   selector: 'app-views',
@@ -30,7 +32,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ViewsComponent implements OnInit{
   @ViewChild(MatTable)
   table!: MatTable<any>;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions', 'doubleWeight'];
   dataSource = ELEMENT_DATA;
 
   constructor(public dialog: MatDialog){ }
@@ -45,7 +47,7 @@ export class ViewsComponent implements OnInit{
       data: element === null ? {
         position: null,
         name: '',
-        weight: null,
+        weight: 'null',
         symbol:''
       } : {
         position: element.position,
@@ -71,6 +73,12 @@ export class ViewsComponent implements OnInit{
   editElement(element: PeriodicElement) : void {
     this.openDialog(element);
   }
+
+ 
+doubleWeight (element: PeriodicElement) : any {
+ element.weight * 2;
+}
+ 
 
   deleteElement(position: number) : void {
     this.dataSource = this.dataSource.filter(p => p.position !== position);
